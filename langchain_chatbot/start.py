6dca5b8acb3e09783bf06e0ad25de43451d4f6f4
dheerendra143 <singhdheerendra83@gmail.com>
2025-09-29 21:07:47 +0530

    Added streamlit chat with application

--- langchain_chatbot/start.py
+++ langchain_chatbot/start.py
@@ -2,8 +2,9 @@ import streamlit as st
 import llm.config as chat
 
 
-st.title("Chat Ollama")
-query = st.text_input("Enter your query")
+st.title("Chat with Ollama")
+st.subheader('This is a small application')
+query = st.text_input(label="Enter your query", placeholder="Write your query here")
 if query:
     # resource = chat.chain.invoke(query)
     # st.write(resource.content)
